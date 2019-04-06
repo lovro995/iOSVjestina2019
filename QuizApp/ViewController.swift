62f39ccae319474b418bbc2e662e8d0fe66be08a
Lovro <lovro-mac@lovros-macbook-pro.local>
2019-04-06 15:31:52 +0200

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Lovro Pejic on 06/04/2019.
+//  Copyright © 2019 Lovro Pejic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
